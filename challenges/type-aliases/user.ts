89a52eb9866d132f801842217277475bebe08c36
ghada <ghadaemad011@gmail.com>
2023-12-29 21:51:13 +0200

    adding solution to type-aliases

--- challenges/type-aliases/user.ts
+++ challenges/type-aliases/user.ts
@@ -3,11 +3,12 @@ type Name = string;
 
 // Now try replacing `unknown` with a primitive data type that might be appropriate for `Year`
 type Year = unknown;
-
+type Count=number;
 type IsOperational = unknown;
-
+type Kilograms=number;
 type Payload = {
   name: Name;
+  mass:Kilograms
 
   // the tests show that you need a `mass` property here
   // but first you might need to create an alias for `Kilograms`
